dc947d1a4913892deca87d591846fb248be31479
José Pedro Ferreira <zepedrocferreira@gmail.com>
2020-07-24 12:44:32 +0100

    Add files via upload

---
+++ 100Questões/newLInt.c
@@ -0,0 +1,9 @@
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
