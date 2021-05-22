20ade7bcc089f958306b75c979bf15957edecf7e
José Pedro Ferreira <zepedrocferreira@gmail.com>
2021-05-22 21:30:18 +0100

    Delete 17.c

--- 100Questões/17.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int maiorPrefixo (char s1 [], char s2 []) {
-    int i = 0, n = 0;
-    for (i=0;s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i];i++){
-        n++;
-    }
-    return n;
-}
